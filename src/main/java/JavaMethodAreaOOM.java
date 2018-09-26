0a65832ffeec1478ff9229d4a47ff3bb8a6c53c7
彭颖 <yingpeng@iflytek.com>
2018-09-26 10:04:41 +0800

    内存溢出异常

---
+++ src/main/java/JavaMethodAreaOOM.java
@@ -0,0 +1,29 @@
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * -XX:PermSize=10M -XX:MaxPermSize=10M
+ */
+public class JavaMethodAreaOOM {
+
+    public static void main(String[] args) {
+        while (true){
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                    return methodProxy.invokeSuper(o,objects);
+                }
+            });
+            enhancer.create();
+        }
+    }
+
+    static class OOMObject{
+
+    }
+}
